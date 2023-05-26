1460db10a2d1a5d32889c38bfb1ec96f113a4db8
Fabio M. Godoy <fabiomdg1@gmail.com>
2023-05-25 22:44:30 -0300

    Implementando login

---
+++ src/main/java/com/br/petshop/domain/usuario/DadosAutenticacao.java
@@ -0,0 +1,4 @@
+package com.br.petshop.domain.usuario;
+
+public record DadosAutenticacao(String login, String senha) {
+}
--- src/main/java/com/br/petshop/infra/security/SecurityConfigurations.java
+++ src/main/java/com/br/petshop/infra/security/SecurityConfigurations.java
@@ -2,9 +2,14 @@ package com.br.petshop.infra.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCrypt;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
@@ -17,6 +22,16 @@ public class SecurityConfigurations {
         return httpSecurity.csrf().disable()
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS)
                 .and().build();
+    }
+
+    @Bean
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration authenticationConfiguration) throws Exception{
+        return authenticationConfiguration.getAuthenticationManager();
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
     }
 
 }
